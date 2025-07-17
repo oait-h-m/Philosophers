770fc329d34d117fe1d35c0178a9d3b7478038bf
oait-h-m <oussamaaithmed1@gmail.com>
2025-07-17 17:23:41 +0100

    add main

--- main.c
+++ main.c
@@ -5,31 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: oait-h-m <oait-h-m@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/03 16:43:00 by oait-h-m          #+#    #+#             */
-/*   Updated: 2025/07/09 18:02:59 by oait-h-m         ###   ########.fr       */
+/*   Created: 2025/07/14 15:37:58 by oait-h-m          #+#    #+#             */
+/*   Updated: 2025/07/14 16:24:24 by oait-h-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	t_program	*info;
-
-	if (pars_arguments(ac, &av[1]) == 0)
-		return (-1);
-	init_program_info(&info, av);
-	if (!info)
-		return (-1);
-	if (info->n_of_philo == 1)
+	t_program	*data;
+	pthread_t	t1;
+	if (pars_arguments(ac, av) == FAILED)
+		return FAILED;
+	if (init_arguments(&data, av) == FAILED || data->n_of_philo == 0)
+		return FAILED;
+	if (data->n_of_philo == 1)
 	{
-		printf("0 1 has taken a fork\n");
-		ft_usleep(info->time_to_die);
-		printf("%ld 1 died\n", info->time_to_die);
-		cleanup(info);
-		return (0);
+		pthread_create(&t1, NULL, one_philo, data);
+		pthread_join(t1, NULL);
 	}
-	start_simulation(info);
-	cleanup(info);
-	return (0);
 }
