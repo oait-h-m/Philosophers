8c10fdb02760ec15144a1ae204566f2e5a0b61a1
oait-h-m <oussamaaithmed1@gmail.com>
2025-07-23 22:40:04 +0100

    add simulation

--- simulation.c
+++ simulation.c
@@ -3,46 +3,97 @@
 /*                                                        :::      ::::::::   */
 /*   simulation.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oait-h-m <oait-h-m@1337.ma>                +#+  +:+       +#+        */
+/*   By: oait-h-m <oait-h-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 07:16:25 by oait-h-m          #+#    #+#             */
-/*   Updated: 2025/07/16 07:18:20 by oait-h-m         ###   ########.fr       */
+/*   Updated: 2025/07/23 21:27:44 by oait-h-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-
 void	*one_philo(void *data)
 {
 	t_program	*info;
 
-	info = (t_program *) data;
+	info = (t_program *)data;
 	printf("0 1 has taken a fork\n");
 	ft_usleep(info->time_to_die);
 	printf("%ld 1 died\n", info->time_to_die);
-	return NULL;
+	return (NULL);
 }
 
-void	*simulation(void *arg)
+void	print_action(t_philo *philo, char *str)
 {
-	return NULL;
+	long	time;
+
+	pthread_mutex_lock(&philo->program->write_mutex);
+	if (!philo->program->is_dead)
+	{
+		time = get_current_time() - philo->program->start_time;
+		printf("%ld %d %s\n", time, philo->id, str);
+	}
+	pthread_mutex_unlock(&philo->program->write_mutex);
 }
 
-void	*routine(t_program *philo)
+void	ft_eat(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->program->forks[philo->l_fork]);
+	print_action(philo, "has taken a fork");
+	pthread_mutex_lock(&philo->program->forks[philo->l_fork]);
+	print_action(philo, "has taken a fork");
+	print_action(philo, "is eating");
+	pthread_mutex_lock(&philo->program->death_mutex);
+	philo->last_meal_time = get_current_time();
+	philo->meals_eaten++;
+	pthread_mutex_unlock(&philo->program->death_mutex);
+	ft_usleep(philo->program->time_to_eat);
+	pthread_mutex_unlock(&philo->program->forks[philo->l_fork]);
+	pthread_mutex_unlock(&philo->program->forks[philo->r_fork]);
+}
+
+void	*routine(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	if (philo->id % 2 == 0)
+		ft_usleep(1);
+	while (!philo->program->is_dead)
+	{
+		ft_eat(philo);
+		if (philo->program->is_dead)
+			break ;
+		print_action(philo, "is sleeping");
+		ft_usleep(philo->program->time_to_sleep);
+		if (philo->program->is_dead)
+			break ;
+		print_action(philo, "is thinking");
+	}
+	return (NULL);
+}
+
+int	simulation(t_program *data)
 {
 	pthread_t	thread;
-	int		i;
+	int			i;
 
-	i= 0;
-	if (philo->number_of_meals == 0)
-		return NULL;
-	if (philo->philo->id % 2 == 0)
-		usleep(1000);
-	while (i < philo->n_of_philo)
+	i = 0;
+	data->start_time = get_current_time();
+	while (i < data->n_of_philo)
 	{
-		pthread_create(&thread, NULL, simulation, arg);
+		data->st_philo[i].last_meal_time = data->start_time;
+		pthread_create(&data->st_philo[i].thread, NULL, routine,
+			&data->st_philo[i]);
 		i++;
 	}
-	return NULL;
+	pthread_create(&thread, NULL, ft_monitor, data);
+	i = 0;
+	while (i < data->n_of_philo)
+	{
+		pthread_join(data->st_philo[i].thread, NULL);
+		i++;
+	}
+	pthread_join(thread, NULL);
+	return (SUCCESS);
 }
