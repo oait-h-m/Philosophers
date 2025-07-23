1dd90a7b6995f0493c35fd8d086eaf9c7265c574
oait-h-m <oussamaaithmed1@gmail.com>
2025-07-23 22:39:23 +0100

    add main

--- main.c
+++ main.c
@@ -12,17 +12,22 @@
 
 #include "philo.h"
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	t_program	*data;
 	pthread_t	t1;
+
 	if (pars_arguments(ac, av) == FAILED)
-		return FAILED;
+		return (FAILED);
 	if (init_arguments(&data, av) == FAILED || data->n_of_philo == 0)
-		return FAILED;
+		return (FAILED);
 	if (data->n_of_philo == 1)
 	{
 		pthread_create(&t1, NULL, one_philo, data);
 		pthread_join(t1, NULL);
+	}
+	else
+	{
+		simulation(data);
 	}
 }
