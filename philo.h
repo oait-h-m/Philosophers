ce17274e69857e90309fdde6245678c64259243b
oait-h-m <oussamaaithmed1@gmail.com>
2025-07-26 17:37:57 +0100

    add header file

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: oait-h-m <oait-h-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 16:43:35 by oait-h-m          #+#    #+#             */
-/*   Updated: 2025/07/23 22:49:48 by oait-h-m         ###   ########.fr       */
+/*   Updated: 2025/07/26 17:34:19 by oait-h-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,16 @@ long						ft_atoi(char *str, int *debug);
 int							init_philo(t_program **data);
 int							init_arguments(t_program **data, char **av);
 long						get_current_time(void);
-void						ft_usleep(long time);
+void						ft_usleep(long time, t_program *data);
 void						*one_philo(void *data);
 void						ft_eat(t_philo *philo);
 int							simulation(t_program *data);
 int							is_simulation_over(t_program *data);
+void						philo_is_die(t_program **data, int i);
+int							is_simulation_over(t_program *data);
+int							check_meals_finished(t_program *data, int i);
+int							check_all_philosophers(t_program *data);
+int							check_philosopher_death(t_program *data, int i);
 void						*ft_monitor(void *arg);
 void						ft_free(t_program *data);
 
