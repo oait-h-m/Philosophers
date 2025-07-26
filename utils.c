1a20a315b23d2a3e4ca6a5e769cc30efdec98492
oait-h-m <oussamaaithmed1@gmail.com>
2025-07-26 17:38:14 +0100

    add utils

--- utils.c
+++ utils.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: oait-h-m <oait-h-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/01 15:21:06 by oait-h-m          #+#    #+#             */
-/*   Updated: 2025/07/23 22:49:20 by oait-h-m         ###   ########.fr       */
+/*   Created: 2025/07/26 17:37:13 by oait-h-m          #+#    #+#             */
+/*   Updated: 2025/07/26 17:37:17 by oait-h-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,13 +44,39 @@ long	get_current_time(void)
 	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
 
-void	ft_usleep(long time)
+void	ft_usleep(long time, t_program *data)
 {
 	long	start;
+	int		is_dead;
 
 	start = get_current_time();
 	while ((get_current_time() - start) < time)
+	{
+		pthread_mutex_lock(&data->death_mutex);
+		is_dead = data->is_dead;
+		pthread_mutex_unlock(&data->death_mutex);
+		if (is_dead)
+			return ;
 		usleep(100);
+	}
+}
+
+int	check_all_philosophers(t_program *data)
+{
+	int	i;
+	int	finish;
+
+	i = 0;
+	finish = 0;
+	while (i < data->n_of_philo && !is_simulation_over(data))
+	{
+		if (check_philosopher_death(data, i))
+			return (FAILED);
+		if (check_meals_finished(data, i))
+			finish++;
+		i++;
+	}
+	return (finish);
 }
 
 void	ft_free(t_program *data)
